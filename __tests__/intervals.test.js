8291081fa417c5516a94fd81a398ee56ac525702
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-18 16:14:19 -0700

    tests working intervals 1 to 7

--- __tests__/intervals.test.js
+++ __tests__/intervals.test.js
@@ -3,14 +3,72 @@ const intervals = require('../intervals');
 describe('Interval tests', () => {
   it('can get a Perfect 1', () => {
     expect(intervals('G', 'P1')).toBe('G');
-    // expect(intervals('F#', 'm2')).toBe('G');
-    // expect(intervals('Gb', 'm2')).toBe('Abb');
+    expect(intervals('Cx', 'P1')).toBe('Cx');
+    expect(intervals('Ebb', 'P1')).toBe('Ebb');
+  });
+  it('can get a minor 2nd', () => {
+    expect(intervals('A#', 'm2')).toBe('B');
+    expect(intervals('Cb', 'm2')).toBe('Dbb');
+    expect(intervals('D', 'm2')).toBe('Eb');
   });
   it('can get a major 2nd', () => {
     expect(intervals('Bb', 'M2')).toBe('C');
     expect(intervals('F#', 'm2')).toBe('G');
     expect(intervals('Gb', 'm2')).toBe('Abb');
   });
+  it('can get a minor 3rd', () => {
+    expect(intervals('E#', 'm3')).toBe('G#');
+    expect(intervals('F', 'm3')).toBe('Ab');
+    expect(intervals('Gx', 'm3')).toBe('B#');
+  });
+  it('can get a major 3rd', () => {
+    expect(intervals('Ab', 'M3')).toBe('C');
+    expect(intervals('Bbb', 'M3')).toBe('Db');
+  });
+  it('can get a perfect 4th', () => {
+    expect(intervals('C', 'P4')).toBe('F');
+    expect(intervals('Dx', 'P4')).toBe('Gx');
+  });
+  it('can get an augmented 4th', () => {
+    expect(intervals('Eb', 'A4')).toBe('A');
+    expect(intervals('F#', 'A4')).toBe('B#');
+  });
+  it('can get an diminished 5th', () => {
+    expect(intervals('G', 'd5')).toBe('Db');
+    expect(intervals('A#', 'd5')).toBe('E');
+  });
+  it('can get a perfect 5th', () => {
+    expect(intervals('B', 'P5')).toBe('F#');
+    expect(intervals('Cbb', 'P5')).toBe('Gbb');
+  });
+  it('can get a minor 6th', () => {
+    expect(intervals('D#', 'm6')).toBe('B');
+    expect(intervals('Ex', 'm6')).toBe('Cx');
+  });
+  it('can get a Major 6th', () => {
+    expect(intervals('F', 'M6')).toBe('D');
+    expect(intervals('Gb', 'M6')).toBe('Eb');
+  });
+  it('can get a minor 7th', () => {
+    expect(intervals('Abb', 'm7')).toBe('Gbb');
+    expect(intervals('B', 'm7')).toBe('A');
+  });
+  it('can get a major 7th', () => {
+    expect(intervals('Cb', 'M7')).toBe('Bb');
+    expect(intervals('D#', 'M7')).toBe('Cx');
+    expect(intervals('F', 'M7')).toBe('E');
+    expect(intervals('Gx', 'M7')).toBe('Fx#');
+  });
+  it.skip('can get a perfect octave', () => {
+    expect(intervals('A#', 'P8')).toBe('A#');
+    expect(intervals('B', 'P8')).toBe('B');
+    expect(intervals('Cb', 'P8')).toBe('Cb');
+    expect(intervals('Dx', 'P8')).toBe('Dx');
+  });
 });
 
+it('can get a aug 1', () => {
+  expect(intervals('C', 'A1')).toBe('C#');
+
+});
 
--- intervals.js
+++ intervals.js
@@ -1,35 +1,44 @@
 const notes = {
-  A: 1,
-  B: 3,
-  C: 4,
-  D: 6,
-  E: 8,
-  F: 9,
-  G: 11,
+  A: 0,
+  B: 2,
+  C: 3,
+  D: 5,
+  E: 7,
+  F: 8,
+  G: 10,
 };
 const intervals = {
   P1: 0,
+  A1: 1,
   m2: 1,
   M2: 2,
   m3: 3,
   M3: 4,
   P4: 5,
-  a4: 6,
+  A4: 6,
   d5: 6,
-  m6: 7,
-  M6: 8,
-  m7: 9,
-  M7: 10,
-  P8: 11,
+  P5: 7,
+  m6: 8,
+  M6: 9,
+  m7: 10,
+  M7: 11,
+  // P8: 0,
 };
 
-module.exports = (note, interval) => {
+
+
+module.exports = (note, interval) => { // Cb, M7
+  // if (!((interval.split('').pop() - 1) % 7)) {
+  //   return note;
+  // }
   let newNote = note.charCodeAt(0) +
     parseInt(interval.split('').pop()) - 1;
   if (newNote > 71) { newNote -= 7; }
-  newNote = String.fromCharCode(newNote);
+  newNote = String.fromCharCode(newNote); // B
+
   let change = 0;
   for (let i = 1; i < note.length; i++) {
+    // refactor to switch
     if (note[i] === 'b') {
       change--;
     }
@@ -40,28 +49,56 @@ module.exports = (note, interval) => {
       change += 2;
     }
   }
-  while (accs(newNote, note, change) > intervals[interval]) {
-    newNote += 'b';
-    change++;
-  }
+  console.log(change);
+  let stop = false;
 
-  let sharpCount = 0;
-  while (accs(newNote, note, change) < intervals[interval]) {
-    sharpCount++;
-    change--;
+  let newNoteNum = parseInt(notes[newNote[0]]);
+  console.log(newNoteNum)
+  let noteNum = parseInt(notes[note[0]]) + change;
+  console.log(noteNum);
+  let intervalNum = intervals[interval];
+
+  // need less than or equal for some
+  if (newNoteNum <= noteNum && note[0] !== newNote[0]) {
+    newNoteNum += 12;
   }
-  let exes = Math.floor(sharpCount / 2);
-  let sharps = sharpCount % 2;
-  for (let i = 0; i < exes; i++) {
-    newNote += 'x';
+  if (change <= 0) {
+    
+    while (accs(newNoteNum, noteNum)  > intervalNum) {
+      stop = true;
+      newNote += 'b';
+      console.log(newNote);
+      noteNum++;
+    }
+
   }
-  for (let i = 0; i < sharps; i++) {
-    newNote += '#';
+  if (!stop) {
+
+    // if (intervalNum === 12 ) {intervalNum = 0;}
+    let sharpCount = 0;
+    while (accs(newNoteNum, noteNum) < intervalNum) {
+      sharpCount++;
+      noteNum--;
+    }
+    let exes = Math.floor(sharpCount / 2);
+    let sharps = sharpCount % 2;
+    for (let i = 0; i < exes; i++) {
+      newNote += 'x';
+    }
+    for (let i = 0; i < sharps; i++) {
+      newNote += '#';
+    }
   }
   return newNote;
 };
 
-const accs = (newNote, note, change) => {
-  let result = notes[newNote[0]] - change - notes[note[0]];
-  return result < 0 ? result + 12 : result;
+const accs = (newNote, note) => {
+  let result = newNote - note;
+  return result;
 };
+// const sharp = (newNote, note, change) => {
+//   let result = notes[newNote[0]] - change - notes[note[0]];
+//   console.log('result', result);
+//   // return result <= 0 ? Math.abs(result) + 12 : result;
+//   return result < 0 ? result + 12 : result;
+// };
