6a344888cfd277b701510f858b16268311c8020f
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-18 03:47:38 -0700

    working with major 2nds

--- __tests__/intervals.test.js
+++ __tests__/intervals.test.js
@@ -1,8 +1,10 @@
 const intervals = require('../intervals');
 
 describe('Interval tests', () => {
-  it('', () => {
-    
+  it('can get a major 2nd', () => {
+    expect(intervals('Bb', 'M2')).toBe('C');
+    expect(intervals('F#', 'm2')).toBe('G');
+    expect(intervals('Gb', 'm2')).toBe('Abb');
   });
 });
 
--- intervals.js
+++ intervals.js
@@ -7,9 +7,24 @@ const notes = {
   F: 9,
   G: 11,
 };
+const intervals = {
+  P1: 0,
+  m2: 1,
+  M2: 2,
+  m3: 3,
+  M3: 4,
+  P4: 5,
+  a4: 6,
+  d5: 6,
+  m6: 7,
+  M6: 8,
+  m7: 9,
+  M7: 10,
+  P8: 11,
+};
 
-module.exports = (note) => {
-  let newNote = note.charCodeAt(0) + 3;
+module.exports = (note, interval) => {
+  let newNote = note.charCodeAt(0) + parseInt(interval[1]) - 1;
   if (newNote > 71) { newNote -= 7; }
   newNote = String.fromCharCode(newNote);
   let change = 0;
@@ -24,13 +39,13 @@ module.exports = (note) => {
       change += 2;
     }
   }
-  while (accs(newNote, note, change) > 5) {
+  while (accs(newNote, note, change) > intervals[interval]) {
     newNote += 'b';
     change++;
   }
 
   let sharpCount = 0;
-  while (accs(newNote, note, change) < 5) {
+  while (accs(newNote, note, change) < intervals[interval]) {
     sharpCount++;
     change--;
   }
