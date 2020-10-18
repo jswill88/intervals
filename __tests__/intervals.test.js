fd9e9c70e980f724af631ebfb93a90de19dfc77d
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-18 11:49:12 -0700

    add a test

--- __tests__/intervals.test.js
+++ __tests__/intervals.test.js
@@ -1,6 +1,11 @@
 const intervals = require('../intervals');
 
 describe('Interval tests', () => {
+  it('can get a Perfect 1', () => {
+    expect(intervals('G', 'P1')).toBe('G');
+    // expect(intervals('F#', 'm2')).toBe('G');
+    // expect(intervals('Gb', 'm2')).toBe('Abb');
+  });
   it('can get a major 2nd', () => {
     expect(intervals('Bb', 'M2')).toBe('C');
     expect(intervals('F#', 'm2')).toBe('G');
