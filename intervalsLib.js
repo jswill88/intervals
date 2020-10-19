2eaea82491f94c3db1210763f5256aa8298678f4
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-18 22:33:00 -0700

    now works with dimished and augmented intervals

---
+++ intervalsLib.js
@@ -0,0 +1,43 @@
+
+/** How far to travel for each interval */
+const intervals = {
+  d1: -1,
+  P1: 0,
+  A1: 1,
+  d2: 0,
+  m2: 1,
+  M2: 2,
+  A2: 3,
+  d3: 2,
+  m3: 3,
+  M3: 4,
+  A3: 5,
+  d4: 4,
+  P4: 5,
+  A4: 6,
+  d5: 6,
+  P5: 7,
+  A5: 8,
+  d6: 7,
+  m6: 8,
+  M6: 9,
+  A6: 10,
+  d7: 9,
+  m7: 10,
+  M7: 11,
+  A7: 12,
+};
+
+/** Notes mapped out to be correctly spaced */
+const notes = {
+  A: 0,
+  B: 2,
+  C: 3,
+  D: 5,
+  E: 7,
+  F: 8,
+  G: 10,
+};
+
+module.exports = { intervals, notes };
+
