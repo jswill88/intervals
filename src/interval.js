04aa679d4094790fd93609ab3db65cceaebae499
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-10-21 00:27:07 -0700

    minor refactoring

--- src/interval.js
+++ src/interval.js
@@ -21,13 +21,13 @@ const invert = require('./invert');
 
 module.exports = (note, interval, direction) => {
   try {
-
-    if(!(typeof note === 'string') || !(typeof interval === 'string')){
+    /** Ensure input types are valid */
+    if (!(typeof note === 'string' && typeof interval === 'string')) {
       throw Error('Note and interval arguments must be strings');
     }
-    
-    if ( direction ) {
-      if(!['up','down','ascending','descending'].includes(direction)) {
+
+    if (direction) {
+      if (!['up', 'down', 'ascending', 'descending'].includes(direction)) {
         throw Error('Direction must be "up", "down", "ascending", or "descending". The default is "up"');
       }
     }
@@ -73,41 +73,39 @@ module.exports = (note, interval, direction) => {
       }
     }
 
-    let stop = false;
-
     let newNoteNum = parseInt(notes[newNote[0]]);
     let noteNum = parseInt(notes[normalizedNote[0]]) + change;
+
+    /**  For doubly diminished and augmented intervals, 
+     * this ensures that an correctly formatted interval 
+     * will used to check against the interval map
+    */
     let baseInterval = scopedInterval[0] + scopedInterval[scopedInterval.length - 1];
     let intervalNum = intervals[baseInterval];
 
     if (newNoteNum <= noteNum && normalizedNote[0] !== newNote[0]) {
       newNoteNum += 12;
     }
-    if (change <= 0) {
 
-      while (newNoteNum - noteNum > intervalNum) {
-        stop = true;
-        newNote += 'b';
-        noteNum++;
-      }
-
+    while (newNoteNum - noteNum > intervalNum) {
+      newNote += 'b';
+      noteNum++;
     }
 
-    if (!stop) {
-      let sharpCount = 0;
-      while (newNoteNum - noteNum < intervalNum) {
-        sharpCount++;
-        noteNum--;
-      }
-      let doubles = Math.floor(sharpCount / 2);
-      let sharps = sharpCount % 2;
-      for (let i = 0; i < doubles; i++) {
-        newNote += 'x';
-      }
-      for (let i = 0; i < sharps; i++) {
-        newNote += '#';
-      }
+    let sharpCount = 0;
+    while (newNoteNum - noteNum < intervalNum) {
+      sharpCount++;
+      noteNum--;
     }
+    let doubles = Math.floor(sharpCount / 2);
+    let sharps = sharpCount % 2;
+    for (let i = 0; i < doubles; i++) {
+      newNote += 'x';
+    }
+    for (let i = 0; i < sharps; i++) {
+      newNote += '#';
+    }
+
     return newNote;
 
   } catch (e) {
