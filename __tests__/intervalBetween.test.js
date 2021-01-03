603a4f15db8da5f8543a56e82ab11ad8a4713b75
Joshua Williams <joshuawilliamsguitar@gmail.com>
2021-01-02 17:12:45 -0800

    write tests for interval between function

--- __tests__/intervalBetween.test.js
+++ __tests__/intervalBetween.test.js
@@ -1,8 +1,43 @@
 const intervalBetween = require('../src/intervalBetween');
 
 describe('Interval between tests', () => {
-  it('can get the correct of half steps for the interval', () => {
-    expect(intervalBetween('G','C')).toBe(5);
-    expect(intervalBetween('Gb','Ax')).toBe(5);
+  it('can handle 1s', () => {
+    expect(intervalBetween('A','A')).toBe('P1');
+    expect(intervalBetween('B','Bb')).toBe('d1');
+    expect(intervalBetween('C', 'C#')).toBe('A1');
+  });
+  it('can handle 2s', () => {
+    expect(intervalBetween('D','Eb')).toBe('m2');
+    expect(intervalBetween('E#', 'Fx')).toBe('M2');
+    expect(intervalBetween('F', 'Gbb')).toBe('d2');
+    expect(intervalBetween('Gb','A#')).toBe('AA2');
+  });
+  it('can handle 3s', () => {
+    expect(intervalBetween('Ab', 'C')).toBe('M3');
+    expect(intervalBetween('Bbb','D')).toBe('A3');
+    expect(intervalBetween('C#','E')).toBe('m3');
+    expect(intervalBetween('D','Fbb')).toBe('dd3');
+  });
+  it('can handle 4s', () => {
+    expect(intervalBetween('E', 'A')).toBe('P4');
+    expect(intervalBetween('F#','B#')).toBe('A4');
+    expect(intervalBetween('Gb','Cbb')).toBe('d4');
+  });
+  it('can handle 5s', () => {
+    expect(intervalBetween('Ab','Eb')).toBe('P5');
+    expect(intervalBetween('B','F')).toBe('d5');
+    expect(intervalBetween('Cb','G#')).toBe('AA5');
+  });
+  it('can handle 6s', () => {
+    expect(intervalBetween('D','B')).toBe('M6');
+    expect(intervalBetween('E#','C#')).toBe('m6');
+    expect(intervalBetween('F','D#')).toBe('A6');
+    expect(intervalBetween('Gb','Ebbb')).toBe('d6');
+  });
+  it('can handle 7s', () => {
+    expect(intervalBetween('A','G#')).toBe('M7');
+    expect(intervalBetween('Bb','A#')).toBe('A7');
+    expect(intervalBetween('C','Bb')).toBe('m7');
+    expect(intervalBetween('D#','C')).toBe('d7');
   });
 });
--- src/intervalBetween.js
+++ src/intervalBetween.js
@@ -9,9 +9,11 @@ const calculateOffset = require('./calculateOffset');
  * interval between them. 
  * 
  * @param { String } note1 The lower note of the 
- * interval to calculate
+ * interval to calculate: 'Bb','C', 'F#', 'Ax'
  * @param { String } note2 The higher note of the 
- * interval to calculate
+ * interval to calculate: 'Bb','C', 'F#', 'Ax'
+ * 
+ * @returns { String } The interval between the two notes. 
  */
 
 
@@ -22,20 +24,52 @@ module.exports = (note1, note2) => {
   // interval number for display
   const noteCode1 = normalizedNote1.charCodeAt(0);
   const noteCode2 = normalizedNote2.charCodeAt(0);
-  const difference = noteCode2 - noteCode1;
-  const numForDisplay = difference < 0 ? difference + 8 : difference;
+  const codeDiff = noteCode2 - noteCode1 + 1;
+  const numForDisplay = codeDiff <= 0 ? codeDiff + 7 : codeDiff;
 
-  const offset = calculateOffset(normalizedNote2,'') - calculateOffset(normalizedNote1,'');
-    
-  // number of half steps between
+  const offset = calculateOffset(normalizedNote2, '') - calculateOffset(normalizedNote1, '');
+
   let noteOneNum = notes[normalizedNote1[0]];
   let noteTwoNum = notes[normalizedNote2[0]];
   if (noteTwoNum < noteOneNum) noteTwoNum += 12;
   let halfStepsBetween = noteTwoNum - noteOneNum + offset;
 
-  // intervals['P4'];
-  // make opposite interval map
-  // find offset of notes 
-  // change interval so that it matches offset
-  return halfStepsBetween;
+  let startingPoint = [1, 4, 5].includes(numForDisplay) ? 'P' : 'M';
+
+  let difference = halfStepsBetween - intervals[`${startingPoint}${numForDisplay}`];
+
+  while (difference > 0) {
+    switch (startingPoint[0]) {
+      case 'P':
+        startingPoint = 'A';
+        break;
+      case 'A':
+        startingPoint += 'A';
+        break;
+      case 'M':
+        startingPoint = 'A';
+        break;
+    }
+    difference--;
+  }
+
+  while (difference < 0) {
+    switch (startingPoint[0]) {
+      case 'P':
+        startingPoint = 'd';
+        break;
+      case 'd':
+        startingPoint += 'd';
+        break;
+      case 'M':
+        startingPoint = 'm';
+        break;
+      case 'm':
+        startingPoint = 'd';
+        break;
+    }
+    difference++;
+  }
+
+  return `${startingPoint}${numForDisplay}`;
 };
